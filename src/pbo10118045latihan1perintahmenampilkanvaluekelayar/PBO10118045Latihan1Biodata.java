7ad60fe373de0b10aa581b9d7c09f3dadb2e9cff
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 20:15:09 +0700

    first commit

---
+++ src/pbo10118045latihan1perintahmenampilkanvaluekelayar/PBO10118045Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan1perintahmenampilkanvaluekelayar;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : menampilkan value ke layar
+ */
+public class PBO10118045Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Biodata Rokstar"+ "\n" + "----------------");
+        System.out.println("Nama\t\t: Muhammad Ilham Apriyadi");
+        System.out.println("Alamat\t\t: Jalan Mendut 4 no. M35");
+        System.out.println("Tempat Lahir\t\t: Bandung");
+        System.out.println("Tanggal Lahir\t\t: 18 April 2000" + "\n" + "----------------");
+        System.out.println("Hobi\t\t: Baca & Nonton");
+        System.out.println("makes\t\t: Sayur Bayam");
+        System.out.println("Mikes\t\t: Jus Melon");
+    }
+    
+}
